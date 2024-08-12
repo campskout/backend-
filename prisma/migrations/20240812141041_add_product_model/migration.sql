2fcad4e15cc68747ba043aacdd01e897ef98c446
Alafajraoui <fullstackjs110299@gmail.com>
2024-08-12 16:22:46 +0200

    Create product , get all products , get by id , delete product , update product done in the backend

---
+++ prisma/migrations/20240812141041_add_product_model/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "ProductCategory" AS ENUM ('hikingEquipment', 'clothing', 'cookingSupplies', 'safetyItems', 'personalCareEssentials');
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "category" "ProductCategory" NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
